e991a3591ff0e33e9996a2fdc183c522264bf91b
Janvi-wb <janvirupapara426@gmail.com>
2024-09-04 07:19:23 +0530

    feat: Edit, Delete, Sorting, Searching

--- ProductsManagementSystem/Controllers/PartiesController.cs
+++ ProductsManagementSystem/Controllers/PartiesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ProductsManagementSystem.DTO;
+using ProductsManagementSystem.Enums;
 using ProductsManagementSystem.ServiceContracts;
 
 namespace ProductManagementSystem.Controllers
@@ -16,10 +18,27 @@ namespace ProductManagementSystem.Controllers
 
 
         [Route("[action]")]
-        public IActionResult Index()
+        [Route("/")]
+        public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PartyResponse.PartyName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
         {
-            List<PartyResponse> parties = _partyService.GetAllParties();
-            return View(parties);
+            //List<PartyResponse> parties = _partyService.GetAllParties();
+            //return View(parties);
+            //Search
+            ViewBag.SearchFields = new Dictionary<string, string>()
+      {
+        { nameof(PartyResponse.PartyName), "Party Name" },
+        { nameof(PartyResponse.PartyCategory), "Category" }
+      };
+            List<PartyResponse> parties = _partyService.GetFilteredParties(searchBy, searchString);
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
+
+            //Sort
+            List<PartyResponse> sortedParties = _partyService.GetSortedParties(parties, sortBy, sortOrder);
+            ViewBag.CurrentSortBy = sortBy;
+            ViewBag.CurrentSortOrder = sortOrder.ToString();
+
+            return View(sortedParties);
         }
 
 
@@ -47,5 +66,71 @@ namespace ProductManagementSystem.Controllers
             //navigate to Index() => parties/index
             return RedirectToAction("Index", "Parties");
         }
+
+        [HttpGet]
+        [Route("[action]/{partyID}")]
+        public IActionResult Edit(Guid partyID)
+        {
+            PartyResponse? partyResponse = _partyService.GetPartyById(partyID);
+
+            if (partyResponse == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            PartyUpdateRequest partyUpdateRequest = partyResponse.ToPartyUpdateRequest();
+
+            return View(partyUpdateRequest);
+        }
+
+
+        [HttpPost]
+        [Route("[action]/{partyID}")]
+        public IActionResult Edit(PartyUpdateRequest partyUpdateRequest)
+        {
+            PartyResponse? partyResponse = _partyService.GetPartyById(partyUpdateRequest.PartyID);
+
+            if (partyResponse == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (ModelState.IsValid)
+            {
+                PartyResponse updatedParty = _partyService.UpdateParty(partyUpdateRequest);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return View(partyResponse.ToPartyUpdateRequest());
+            }
+        }
+
+
+        [HttpGet]
+        [Route("[action]/{partyID}")]
+        public IActionResult Delete(Guid? partyID)
+        {
+            PartyResponse? partyResponse = _partyService.GetPartyById(partyID);
+            if (partyResponse == null)
+                return RedirectToAction("Index");
+
+            return View(partyResponse);
+        }
+
+        [HttpPost]
+        [Route("[action]/{partyID}")]
+        public IActionResult Delete(PartyUpdateRequest
+            partyUpdateRequest)
+        {
+            PartyResponse? partyResponse = _partyService.GetPartyById(partyUpdateRequest.PartyID);
+            if (partyResponse == null)
+                return RedirectToAction("Index");
+
+            _partyService.DeleteParty(partyUpdateRequest.PartyID);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- ProductsManagementSystem/Services/PartyService.cs
+++ ProductsManagementSystem/Services/PartyService.cs
@@ -1,6 +1,9 @@
-﻿using ProductManagementSystem.Models;
+﻿using Azure.Core;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ProductManagementSystem.Models;
 using ProductsManagementSystem.Data;
 using ProductsManagementSystem.DTO;
+using ProductsManagementSystem.Enums;
 using ProductsManagementSystem.RepositoryContracts;
 using ProductsManagementSystem.ServiceContracts;
 using System;
@@ -43,10 +46,111 @@ namespace ProductsManagementSystem.Services
                 .Select(temp => ConvertPartyToPartyResponse(temp)).ToList();
         }
 
+        public PartyResponse? GetPartyById(Guid? PartyID)
+        {
+            if (PartyID == null)
+                return null;
+
+            Party? party = _db.Parties
+                .FirstOrDefault(temp => temp.PartyID == PartyID);
+            if (party == null)
+                return null;
+
+            return ConvertPartyToPartyResponse(party);
+        }
+
+        public PartyResponse UpdateParty(PartyUpdateRequest? partyUpdateRequest)
+        {
+            Party? party = _db.Parties.Find(partyUpdateRequest?.PartyID);
+
+            if (party == null)
+            {
+                throw new KeyNotFoundException("Party not found");
+            }
+
+            //update all details
+            party.PartyName = partyUpdateRequest.PartyName;
+            party.PartyCategory = partyUpdateRequest.PartyCategory;
+
+            _db.SaveChanges();
+
+            return party.ToPartyResponse();
+        }
+
+        public bool DeleteParty(Guid? partyID)
+        {
+            if (partyID == null)
+            {
+                throw new ArgumentNullException(nameof(partyID));
+            }
+
+            Party? person = _db.Parties.FirstOrDefault(temp => temp.PartyID == partyID);
+            
+            if (person == null)
+                return false;
+
+            _db.Parties
+                .Remove(_db.Parties.First(temp => temp.PartyID == partyID));
+            _db.SaveChanges(); 
+
+            return true;
+        }
+
         private PartyResponse ConvertPartyToPartyResponse(Party party)
         {
             PartyResponse partyResponse = party.ToPartyResponse();
             return partyResponse;
         }
+
+        public List<PartyResponse> GetFilteredParties(string searchBy, string? searchString)
+        {
+            List<PartyResponse> parties = GetAllParties();
+            List<PartyResponse> matchingParties = parties;
+
+            if (string.IsNullOrEmpty(searchBy) || string.IsNullOrEmpty(searchString))
+                return matchingParties;
+
+
+            switch (searchBy)
+            {
+                case nameof(PartyResponse.PartyName):
+                    matchingParties = parties.Where(temp =>
+                    (!string.IsNullOrEmpty(temp.PartyName) ?
+                    temp.PartyName.Contains(searchString, StringComparison.OrdinalIgnoreCase) : true)).ToList();
+                    break;
+
+                case nameof(PartyResponse.PartyCategory):
+                    matchingParties = parties.Where(temp =>
+                    (!string.IsNullOrEmpty(temp.PartyCategory) ?
+                    temp.PartyCategory.Contains(searchString, StringComparison.OrdinalIgnoreCase) : true)).ToList();
+                    break;
+
+                default: matchingParties = parties; break;
+            }
+            return matchingParties;
+        }
+
+        public List<PartyResponse> GetSortedParties(List<PartyResponse> parties, string sortBy, SortOrderOptions sortOrder)
+        {
+            if (string.IsNullOrEmpty(sortBy))
+                return parties;
+
+            List<PartyResponse> sortedParties = (sortBy, sortOrder) switch
+            {
+                (nameof(PartyResponse.PartyName), SortOrderOptions.ASC) => parties.OrderBy(temp => temp.PartyName, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PartyResponse.PartyName), SortOrderOptions.DESC) => parties.OrderByDescending(temp => temp.PartyName, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PartyResponse.PartyCategory), SortOrderOptions.ASC) => parties.OrderBy(temp => temp.PartyCategory, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PartyResponse.PartyCategory), SortOrderOptions.DESC) => parties.OrderByDescending(temp => temp.PartyCategory, StringComparer.OrdinalIgnoreCase).ToList(),
+
+
+                _ => parties
+            };
+
+            return sortedParties;
+        }
     }
-}
+ }
+
